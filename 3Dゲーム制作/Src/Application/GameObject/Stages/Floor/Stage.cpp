05684bba7d7045b6bb55d70539fe1c54d26a19bb
kd1382103 <kd1382103@st.kobedenshi.ac.jp>
2026-08-27 11:59:36 +0900

    一旦ステージ描画のみ、一部建物に色がつかない不具合があるが一旦このまま
    次はPlayerのモデルだがすでに重いため、軽量化処理を作りたい

--- 3Dゲーム制作/Src/Application/GameObject/Stages/Floor/Stage.cpp
+++ 3Dゲーム制作/Src/Application/GameObject/Stages/Floor/Stage.cpp
@@ -5,7 +5,8 @@ void Stage::Init()
 	if (!m_model)
 	{
 		m_model = std::make_shared<KdModelData>();
-		m_model->Load("Asset/Models/stages/Stage/stage.gltf");
+		//m_model->Load("Asset/Models/stages/Stage/stage.gltf");
+		m_model->Load("Asset/Models/stages/Stage2/StageMap/stageMap.gltf");
 
 		m_pCollider = std::make_unique<KdCollider>();
 		m_pCollider->RegisterCollisionShape("Stage", m_model, KdCollider::TypeGround | KdCollider::TypeBump);
@@ -17,5 +18,3 @@ void Stage::DrawLit()
 	if (!m_model) { return; }
 	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_model);
 }
-
-
